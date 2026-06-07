263c0d1351a3b59e53f20bd37ae334fd8cf4e0fe
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-06-07 12:08:44 +0530

    Update Employee_Bonus.py

--- Conditional_Statements/Employee_Bonus.py
+++ Conditional_Statements/Employee_Bonus.py
@@ -1,9 +1,18 @@
-service_year = int(input("How many year you have done your service in this company? "))
+service_year = int(input("How many years have you worked in this company? "))
 emp_salary = int(input("Enter your salary: "))
 bonus = 0
+
 if service_year > 5:
-    bonus = emp_salary * 0.1
+    if emp_salary < 20000:
+        bonus = emp_salary * 0.2
+    elif 20000 < emp_salary < 50000:
+        bonus = emp_salary * 0.1
+    else:
+        bonus = emp_salary * 0.05
+        
     emp_salary += bonus
-    print(f"Including bonus your salary is {emp_salary}")
+    
+    print(f"Bonus Amount: {bonus}")
+    print(f"Including bonus final salary is {emp_salary}")
 else:
     print("Sorry! you are not eligible for bonus.")
