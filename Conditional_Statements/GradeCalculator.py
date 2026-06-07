7df34db9357f6890ba6011088efcc3d36fe38b1a
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-06-07 12:08:03 +0530

    Update GradeCalculator.py

--- Conditional_Statements/GradeCalculator.py
+++ Conditional_Statements/GradeCalculator.py
@@ -1,6 +1,8 @@
 student_mark = int(input("Enter your mark here: "))
 
-if student_mark >= 90:
+if student_mark < 0 or student_mark > 100:
+    print("Invalid Marks")
+elif student_mark >= 90:
     print("A grade")
 elif 80 <= student_mark < 90:
     print("B grade")
@@ -10,3 +12,4 @@ elif 60 <= student_mark < 70:
     print("D grade")
 else:
     print("Fail")
+
