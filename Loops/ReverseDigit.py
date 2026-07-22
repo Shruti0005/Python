f469f25fd7a932c82f9a63f177ee5e2d287ea164
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-07-22 18:24:35 +0530

    Update ReverseDigit.py
    changes in code

--- Loops/ReverseDigit.py
+++ Loops/ReverseDigit.py
@@ -1,10 +1,11 @@
 #Reverse an integer (e.g., 12345 → 54321).
-given_number = int(input("Enter number: "))
-n = given_number
+def reverse(num):
+    res = 0
+    while num > 0:
+        rem = num % 10
+        res = res * 10 + rem
+        num //= 10
+    return res
 
-reverse_digit = 0
-while(n > 0):
-    digit = n % 10
-    reverse_digit = (reverse_digit * 10) + digit
-    n //= 10
-print(reverse_digit)
+number = int(input("Enter number: "))
+print(f"Reverse number: {reverse(number)}")
