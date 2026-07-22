ad15fb92e5c9d5047ff450ac411dc5bcc7f4dd8e
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-07-22 18:26:54 +0530

    Update SumOfDigit.py
    some updates in files

--- Loops/SumOfDigit.py
+++ Loops/SumOfDigit.py
@@ -1,10 +1,11 @@
 #Calculate the sum of digits of a number.
-given_number = int(input("Enter number: "))
-n = given_number
+def sum_digit(num):
+    total = 0
+    while num > 0:
+        rem = num % 10
+        total += rem
+        num //= 10
+    return total
 
-total = 0
-while(n > 0):
-    digit = n % 10
-    total += digit
-    n //= 10
-print(f"Summation of digit: {total}")
+number = int(input("Enter number: "))
+print(f"Summation of digit: {sum_digit(number)}")
