102cc64548e61cc490f4b0f6b6009fc586ab0ac3
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-07-22 18:25:40 +0530

    Update FactorialOfNum.py
    some updates in code

--- Loops/FactorialOfNum.py
+++ Loops/FactorialOfNum.py
@@ -1,7 +1,9 @@
 #Find the factorial of a given number (e.g., 5! = 120).
-given_num = int(input("Enter the number: "))
-fact = 1
-for number in range(1, given_num + 1):
-    fact *= number
+def factorial(num):
+    fact = 1
+    for i in range(1, num+1):
+        fact *= i
+    return fact
 
-print(fact)
+number = int(input("Enter number: "))
+print(f"Factorial: {factorial(number)}")
